e3b08f94673fc0d511df6c52ff2ade9008241782
Job Oboth <trappsavage601@gmail.com>
2025-11-08 21:15:18 +0300

    Created a new function "getParams" and updated the product ids

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,10 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import { getParam } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
+
 
 function addProductToCart(product) {
   const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
