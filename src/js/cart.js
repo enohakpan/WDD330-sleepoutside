4cba31c30c895cf885785e46e190afbfd3c50d20
Job Oboth <trappsavage601@gmail.com>
2025-11-22 10:58:04 +0300

    Added the remove from cart feature

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,28 +1,36 @@
 import { getLocalStorage } from "./utils.mjs";
 
+
+// RENDER CART CONTENTS
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 
+// Delete button included
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: ${item.quantity || 1}</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+  return `
+    <li class="cart-card divider">
+      <a href="#" class="cart-card__image">
+        <img src="${item.Image}" alt="${item.Name}" />
+      </a>
 
-  return newItem;
+      <a href="#">
+        <h2 class="card__name">${item.Name}</h2>
+      </a>
+
+      <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+      <p class="cart-card__quantity">qty: ${item.quantity || 1}</p>
+      <p class="cart-card__price">$${item.FinalPrice}</p>
+
+      <!-- DELETE BUTTON -->
+      <button class="remove-btn" data-id="${item.Id}">
+        Remove
+      </button>
+    </li>
+  `;
 }
 
+// Run the cart render
 renderCartContents();
